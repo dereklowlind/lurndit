b2ad43c7bce3882ae7bf5a535f0994653c3a0944
dereklowlind <dereklowlind@gmail.com>
2020-12-28 15:38:06 -0800

    put signin into a modal that is now triggered by a siginin/signup button

--- src/molecules/HeaderBar.js
+++ src/molecules/HeaderBar.js
@@ -1,8 +1,19 @@
 import Auth from './Auth'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    headerContainer: {
+        display: 'flex',
+        justifyContent: 'flex-end'
+    }
+}))
 
 function HeaderBar() {
+    const classes = useStyles();
+
     return(
-    <div>
+    <div className={classes.headerContainer}>
+        <div>Search bar goes here</div>
         <Auth />
     </div>
     )
