4961b9932193ac1fa8f57669f11822b02d8e9f34
dereklowlind <dereklowlind@gmail.com>
2021-01-14 12:50:56 -0800

    removed facebook auth and deployed

--- src/molecules/Auth.js
+++ src/molecules/Auth.js
@@ -15,7 +15,7 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID
   ],
   callbacks: {
     // Avoid redirects after sign-in.
